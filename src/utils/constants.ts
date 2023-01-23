f5867392eaa817e7a3a0bdb6ded6731946335a65
gingaminga <gingahae@gmail.com>
2023-01-23 17:42:39 +0900

    🚚Style: 상수값 변경

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export default {
   LOG_PATH: process.env.LOG_PATH,
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: Number(process.env.PORT || 3001), // 서버 포트
-  PROJECT_NAME: process.env.PROJECT_NAME || "Sample Project Name",
+  PROJECT_NAME: process.env.PROJECT_NAME || "LIP",
   REDIS: {
     HOST: process.env.REDIS_HOST || "127.0.0.1",
     PASSWORD: process.env.REDIS_PASS || "",
