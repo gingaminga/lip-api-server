e47fbc1dfdd893f2114326976f41b8247d53708e
gingaminga <gingahae@gmail.com>
2023-04-27 01:19:38 +0900

    ✨ Feat: 랜덤 텍스트 가져오는 util 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,24 @@
 import { RESPONSE_STATUS } from "@/utils/response";
 
 /**
+ * @description 무작위 텍스트 만들기
+ * @param length 만들고자하는 길이
+ * @returns 무작위 문자열
+ */
+export const getRandomText = (length = 15) => {
+  const CHARACTER = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  const test = CHARACTER.length;
+
+  let randomstring = "";
+  for (let i = 0; i < length; i++) {
+    const rnum = Math.floor(Math.random() * test);
+    randomstring += CHARACTER.substring(rnum, rnum + 1);
+  }
+
+  return randomstring;
+};
+
+/**
  * @description 응답 포맷
  * @param status 성공 실패 여부
  * @param data 전달할 데이터
