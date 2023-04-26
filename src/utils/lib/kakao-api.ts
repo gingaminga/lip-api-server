cb04c2bec1a7efb2560885992c8ef5c83d6c0a53
gingaminga <gingahae@gmail.com>
2023-04-27 01:20:01 +0900

    ✨ Feat: Kakao auth class 추가

---
+++ src/utils/lib/kakao-api.ts
@@ -0,0 +1,87 @@
+import { KAKAO_URL } from "@/utils/lib/url";
+import { AxiosBase } from "axios-classification";
+
+interface IUserDataPartner {
+  uuid: string;
+}
+
+interface IUserDataAccountProfile {
+  is_default_image?: boolean;
+  nickname?: string;
+  profile_image_url?: string;
+  thumbnail_image_url?: string;
+}
+
+interface IUserDataProperties {
+  nickname: string;
+}
+
+interface IUserDataAccount {
+  age_range_needs_agreement?: boolean;
+  age_range?: string; // 연령대
+  birthday?: string; // 생일 (MMDD)
+  birthday_needs_agreement?: boolean;
+  birthday_type?: string; // 생일 타입 (양력/음력)
+  birthyear?: string; // 출생연도 (YYYY 형식)
+  birthyear_needs_agreement?: boolean;
+  ci?: string;
+  ci_authenticated_at?: Date;
+  ci_needs_agreement?: boolean;
+  email?: string;
+  email_needs_agreement?: boolean;
+  gender?: string;
+  gender_needs_agreement?: boolean;
+  has_email?: boolean;
+  is_email_valid?: boolean;
+  is_email_verified?: boolean;
+  name?: string; // 닉네임
+  name_needs_agreement?: boolean;
+  phone_number?: string;
+  phone_number_needs_agreement?: boolean;
+  profile?: IUserDataAccountProfile;
+  profile_image_needs_agreement?: boolean;
+  profile_needs_agreement?: boolean;
+  profile_nickname_needs_agreement?: boolean;
+}
+
+interface IUserData {
+  connected_at?: Date;
+  for_partner?: IUserDataPartner;
+  has_signed_up?: boolean;
+  id: number;
+  kakao_account?: IUserDataAccount;
+  properties?: IUserDataProperties;
+  synched_at?: Date;
+}
+
+class KakaoApi extends AxiosBase {
+  /**
+   * @description 헤더에 액세스토큰 설정하기
+   * @param token 액세스토큰
+   */
+  setAccessTokenInHeader(token: string) {
+    this.setBearerToken(token);
+  }
+
+  /**
+   * @description 유저 정보 가져오기
+   * @param token 액세스토큰
+   */
+  async getUserInfo() {
+    const { data } = await this.post<null, IUserData>(KAKAO_URL.API.PATH.USER_DATA, null);
+    const { id, kakao_account: kakaoAccount } = data;
+    const { profile } = kakaoAccount || {};
+    const { nickname = "" } = profile || {};
+
+    const userInfo = {
+      id,
+      nickname,
+    };
+
+    return userInfo;
+  }
+}
+
+export const KakaoApiClient = new KakaoApi({
+  baseURL: KAKAO_URL.API.HOST,
+});
--- src/utils/lib/url.ts
+++ src/utils/lib/url.ts
@@ -1,4 +1,10 @@
 export const KAKAO_URL = {
+  API: {
+    HOST: "https://kapi.kakao.com",
+    PATH: {
+      USER_DATA: "/v2/user/me",
+    },
+  },
   AUTH: {
     HOST: "https://kauth.kakao.com",
     PATH: {
