d193051ae4db317fe08271128f6cf957fdf171df
gingaminga <gingahae@gmail.com>
2023-08-02 15:57:48 +0900

    🔊 Feat: pm2 log 쌓기
    pm2 logrotate가 winston dailylotate보다 세밀하고 정확함
    winton logger는 주기적으로 삭제하도록 설정 변경 예정(추후 로직 제거)

--- ecosystem.development.config.js
+++ ecosystem.development.config.js
@@ -5,8 +5,8 @@ module.exports = {
         name: "lip-api-server-development", // 이름
         script: "./dist/index.js", // 실행 파일
         watch: false, // 실시간 변경 확인 여부
-        out_file: "/dev/null", // 로그 파일 경로
-        error_file: "/dev/null", // 에러 로그 파일 경로
+        out_file: "/logs/development/lip-api-server/lip-api-server-out.log", // 로그 파일 경로
+        error_file: "/logs/development/lip-api-server/lip-api-server-error.log", // 에러 로그 파일 경로
       },
     ],
   };
