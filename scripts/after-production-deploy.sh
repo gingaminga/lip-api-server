d41d0e0a41ffe533f9b07a0e3cfff4c67dbce4fa
gingaminga <gingahae@gmail.com>
2023-08-02 16:27:27 +0900

    👷 Ci: 개발/운영 서비스 분리
    개발/운영 서비스를 분리하여 CI/CD가 되도록 처리

--- scripts/after-production-deploy.sh
+++ scripts/after-production-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ROOT_REPOSITORY=/home/ubuntu
-BASE_REPOSITORY=/lip-api-server
+BASE_REPOSITORY=/production/lip-api-server
 
 # move directory
 cd $ROOT_REPOSITORY$BASE_REPOSITORY
 
 # start service
-pm2 start ecosystem.config.js
+pm2 start ecosystem.production.config.js
